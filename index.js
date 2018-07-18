1cb059ad779b269f0e0a0bec289d638e797f2350
Fida Muntaseer <fida1989@users.noreply.github.com>
2018-07-19 01:40:16 +0600

    IP updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var fs = require('fs');
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var x;
 
