82ce07c87770de7ffb88db389ccc58b3f8ccd27a
fida1989 <fida.muntaseer.1989@gmail.com>
2018-12-25 02:37:41 +0600

    Code updates

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-var http = require('http');
+const express = require('express')
+const app = express()
 var fs = require('fs');
 var x;
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
-http.createServer(function (req, res) {
+app.get('/', function (req, res) {
   fs.readFile('facts.json', function (err, data) {
-    var myJSON = JSON.parse(data);
-    x = Math.floor((Math.random() * myJSON.data.length));
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.write(JSON.stringify(myJSON.data[x]));
-    res.end();
-  });
-}).listen(PORT, () => console.log(`Listening on ${PORT}`));  
+    if (err) {
+      res.send("Error!")
+    } else {
+        var myJSON = JSON.parse(data);
+        x = Math.floor((Math.random() * myJSON.data.length));
+        res.send(JSON.stringify(myJSON.data[x]));
+    }
+  })
+})
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
