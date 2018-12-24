4b7fcef41403e20b39ec863dfde5d99a6b1dd0d6
fida1989 <fida.muntaseer.1989@gmail.com>
2018-12-24 22:11:51 +0600

    index.js updated

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var fs = require('fs');
 var x;
 const PORT = process.env.PORT || 5000
 
-http.createServer(function (req, res) {  
-   fs.readFile('facts.json', function(err, data) {
+http.createServer(function (req, res) {
+  fs.readFile('facts.json', function (err, data) {
     var myJSON = JSON.parse(data);
     x = Math.floor((Math.random() * myJSON.data.length));
-    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.write(JSON.stringify(myJSON.data[x]));
-    res.end(); 
+    res.end();
   });
-}).listen(PORT);  
+}).listen(PORT, () => console.log(`Listening on ${PORT}`));  
