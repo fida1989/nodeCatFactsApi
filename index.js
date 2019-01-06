045a8b172b25da8afa24fa6e63050a08225d399f
fida1989 <fida.muntaseer.1989@gmail.com>
2019-01-07 01:25:39 +0600

    Major updates

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-var fs = require('fs');
-var x;
 const port = process.env.PORT || 5000
+const db = 'mongodb://fida:fida1989@cluster0-shard-00-00-zgjze.mongodb.net:27017,cluster0-shard-00-01-zgjze.mongodb.net:27017,cluster0-shard-00-02-zgjze.mongodb.net:27017/AppData?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+
+mongoose.connect(db, {useNewUrlParser: true});
+
+const Cat = mongoose.model('Cat', { name: String });
 
 app.get('/', function (req, res) {
-  fs.readFile('facts.json', function (err, data) {
+  Cat.find({}).exec(function(err,facts){
     if (err) {
-      res.send("Error!")
-    } else {
-        var myJSON = JSON.parse(data);
-        x = Math.floor((Math.random() * myJSON.data.length));
-        res.send(JSON.stringify(myJSON.data[x]));
-    }
-  })
-})
+      console.log(err);
+      res.send('Error');
+  } else {
+      console.log(facts.length);
+      const x = Math.floor((Math.random() * facts.length));
+      res.json(facts[x]);
+  }
+  });
+});
+
+
+
+
+
 
 app.listen(port, () => console.log(`listening on port ${port}!`))
